f785143a9fa8bc13a2c44c28c90752c9f9b30af9
Pavel Gurenko <pgurenko@gmail.com>
2017-11-28 20:04:50 -0800

    initial thoughts on basic class structure

--- pawnpy/src/main.cpp
+++ pawnpy/src/main.cpp
@@ -1,12 +1,66 @@
+//-----------------------------------------------------------------------------
+#include <iostream>
 #include <boost/python.hpp>
-
-char const* greet()
-{
-   return "hello, world";
+//-----------------------------------------------------------------------------
+namespace pawn {
+#include "pawn/compiler/sc.h"
 }
+//-----------------------------------------------------------------------------
+using namespace std;
+using namespace boost::python;
+//-----------------------------------------------------------------------------
+
+class File
+{
+public:
+  File(const std::string& filename)
+    : _filename(filename) {
+    cout << __FUNCTION__ <<  " filename=" << _filename << endl;
+  }
+private:
+  std::string _filename;
+};
+//-----------------------------------------------------------------------------
+
+class AMX : public File
+{
+public:
+  AMX(const std::string& filename)
+    : File(filename) {
+  }
+};
+//-----------------------------------------------------------------------------
+
+class Source : public File
+{
+public:
+  Source(const std::string& filename)
+    : File(filename)
+  {
+  }
+
+  int compile()
+  {
+    cout << __FUNCTION__ << endl;
+    // filling proper argc and argv based on parameters
+    return pawn::pc_compile(0, NULL);
+  }
+
+  int set_breakpoint(uint32_t line) {
+    cout << __FUNCTION__ << " - line " << line << endl;
+    return true;
+  }
+};
+//-----------------------------------------------------------------------------
 
 BOOST_PYTHON_MODULE(pawnpy)
 {
-    using namespace boost::python;
-    def("greet", greet);
+    class_<Source>("Source", init<std::string>())
+        .def("compile", &Source::compile)
+        .def("set_breakpoint", &Source::set_breakpoint)
+        ;
+
+    class_<AMX>("AMX", init<std::string>())
+        ;
 }
+//-----------------------------------------------------------------------------
