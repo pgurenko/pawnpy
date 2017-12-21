f453f0a35b582986879e4e8829d17a34995d1062
Pavel Gurenko <pgurenko@gmail.com>
2017-12-20 22:02:29 -0800

    working on build

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ cd build
 cmake ../pawnpy
 make pawncc pawnpy -j4
 cd ../
+
+cp -f build/libpawnpy.so ./pawnpy
+cp -f build/pawncc ./pawnpy
--- runtests.sh
+++ runtests.sh
@@ -2,9 +2,6 @@
 
 ./build.sh
 
-cp -f build/libpawnpy.so ./pawnpy
-cp -f build/pawncc ./pawnpy
-
 echo "Running $(python3 --version)"
 
 python3 -m unittest discover ./tests
