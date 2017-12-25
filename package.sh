8b6866bdff27ad367933e4aebf5497623865e452
Pavel Gurenko <pgurenko@gmail.com>
2017-12-24 21:00:12 -0800

    debugging deploy

--- package.sh
+++ package.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 DOWNLOAD_URI=https://github.com/pgurenko/pawnpy/releases/download/$(git describe)
 curl $DOWNLOAD_URI/libpawnpy.linux.x64.so -o ./pawnpy/libpawnpy.linux.x64.so
 curl $DOWNLOAD_URI/pawncc.linux.x64 -o ./pawnpy/pawncc.linux.x64
+curl $DOWNLOAD_URI/libpawnpy.osx.x64.so -o ./pawnpy/libpawnpy.osx.x64.so
+curl $DOWNLOAD_URI/pawncc.osx.x64 -o ./pawnpy/pawncc.osx.x64
 
 tar -zcvf pawnpy-$(git describe).tar.gz setup.py pawnpy/*.py pawnpy/*.so pawnpy/pawncc*
